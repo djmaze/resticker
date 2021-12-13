11f2b1b2f0cc047aba645fd45b129522d3fca438
Martin Honermeyer <maze@strahlungsfrei.de>
2021-12-13 01:52:10 +0100

    Run failure command if backup is incomplete and incomplete command is not set

--- spec/backup_spec.sh
+++ spec/backup_spec.sh
@@ -64,7 +64,18 @@ HERE
     The status should eq 1
   End
 
-  It "Runs incomplete command if backup is incomplete"
+  It "Runs failure command if backup is incomplete and incomplete command is not specified"
+    cat <<HERE >"$extra_env"
+      RESTIC_BACKUP_SOURCES=/proc/self/mem
+      POST_COMMANDS_FAILURE=echo Total failure!
+HERE
+    When call docker_exec backup
+    The stderr should not include "Fatal:"
+    The output should include "Total failure!"
+    The status should eq 3
+  End
+
+  It "Runs incomplete command if backup is incomplete and incomplete command is specified"
     cat <<HERE >"$extra_env"
       RESTIC_BACKUP_SOURCES=/proc/self/mem
       POST_COMMANDS_INCOMPLETE=echo Partial failure!
